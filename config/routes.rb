fba44f5cddcdb01a4e18c9f616b32bb08e2b812a
OmineKaori <minemusic@icloud.com>
2019-11-29 16:26:30 +0900

    新規登録できるようになったよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'user/index' => "user#index"
+  get 'users/index' => "users#index"
+  get "users/signup" => "users#new"
+  get "users/:id" => "users#show"
+  post "users/create" => "users#create"
+
   get 'posts/index' => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
+
   get "/" => "home#top"
   get "about" => "home#about"
 
