3a1b3aa1486ef52578ced71b41423d4f6ec55ba1
OmineKaori <minemusic@icloud.com>
2019-12-02 14:58:47 +0900

    大まかに完成した！

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true} #ちゃんとemailの形式になっているかあとで実装
   validates :password, {presence: true}
+
+  def posts
+    return Post.where(user_id:self.id)
+  end
 end
